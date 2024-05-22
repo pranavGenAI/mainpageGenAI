4155c3a5769ebba40d300fb8fb961cb58fb16b15
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-22 12:56:09 +0530

    main.py

--- main.py
+++ main.py
@@ -71,11 +71,56 @@ col1, col2, col3 = st.columns([1, 1, 1])
 
 with col1:
     st.markdown("""
-        <img src="https://github.com/pranavGenAI/mainpageGenAI/blob/main/bidbooster.png" width="400" class="glow-on-hover">
+        <a href="https://chatwithbidbooster.streamlit.app/">
+            <img src="https://lh3.googleusercontent.com/drive-viewer/AKGpihaiLwB5hnQjwXzS8fm14y2v4-Hq2ZooZG2zqdWCmAk-sF2_h0N_aDpRWHLRkwtVkk0cL_Sq7pAiAhutX2pQQBqtyxmiqVVskQ=s1600-rw-v1" class="glow-on-hover" height=350 width=450>
+        </a>
     """, unsafe_allow_html=True)
 
 with col2:
-    st.write("Col2")
+    st.markdown("""
+        <a href="https://bidboosterjpn.streamlit.app/">
+            <img src="https://lh3.googleusercontent.com/drive-viewer/AKGpihZu2SSBe5wM3ocA0WfBtlAjR-lwQ2Tn2CZAJaUQnAVjKfT6YeGzcN4cKSwGCwJQ4ImfFHrEOix8t2ul4LecjUh3fD1YURJB3S4=s1600-rw-v1" class="glow-on-hover" height=350 width=450>
+        </a>
+    """, unsafe_allow_html=True)
 
 with col3:
-    st.write("Col3")
+    st.markdown("""
+        <a href="https://datavizanalyst.streamlit.app/">
+            <img src="https://lh3.googleusercontent.com/drive-viewer/AKGpihYdnKU3WUMDKWU7-tUvo2Kka73CmAC5f8UGXxHeiPRkJZMtdAp21HDzg998B9nX65OAw2waL5c6qrQ0Vq82Zn4j_OgdzKyH3co=s1600-rw-v1" class="glow-on-hover" height=350 width=450>
+        </a>
+    """, unsafe_allow_html=True)
+
+
+
+st.markdown('''<style>
+    .stApp > header {
+        background-color: transparent;
+    }
+    .stApp {
+        background: linear-gradient(45deg, #092035 20%, #0E1117 45%, #0E1117 55%, #3a5683 90%);
+        animation: my_animation 20s ease infinite;
+        background-size: 200% 200%;
+        background-attachment: fixed;
+    }
+    @keyframes my_animation {
+        0% {background-position: 0% 0%;}
+        50% {background-position: 100% 100%;}
+        100% {background-position: 0% 0%;}
+    }
+    [data-testid=stSidebar] {
+        background: linear-gradient(360deg, #1a2631 95%, #161d29 10%);
+    }
+    div.stButton > button:first-child {
+        background: linear-gradient(45deg, #c9024b 45%, #ba0158 55%, #cd006d 70%);
+        color: white;
+        border: none;
+    }
+    div.stButton > button:hover {
+        background: linear-gradient(45deg, #ce026f 45%, #970e79 55%, #6c028d 70%);
+        background-color: #ce1126;
+    }
+    div.stButton > button:active {
+        position: relative;
+        top: 3px;
+    }    
+</style>''', unsafe_allow_html=True)
