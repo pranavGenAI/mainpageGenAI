2e5286eed84ccaed7b09a408ac31ba59dba1bb32
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-22 11:21:40 +0530

    main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ col1, col2, col3 = st.columns([1, 1, 1])
 
 with col1:
     st.markdown("""
-        <img src="C:/Users/pranav.baviskar/Desktop/Learning/GenAI/BidBooster/bidbooster.png" width="400" class="glow-on-hover">
+        <img src="/bidbooster.png" width="400" class="glow-on-hover">
     """, unsafe_allow_html=True)
 
 with col2:
