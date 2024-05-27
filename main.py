0d1b9b2ddf3037d9274174ef6464721e7746292f
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-27 11:30:51 +0530

    main.py

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ html(
       tsParticles.load("particles", {
          particles: {
             number: {
-               value: 80
+               value: 90
             },
             size: {
-               value: 2.5  // Adjust the particle size here (default is 3)
+               value: 3  // Adjust the particle size here (default is 3)
             },
             move: {
                enable: true,
