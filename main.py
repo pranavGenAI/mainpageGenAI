9cc70a53b9ed46117e6f2d8cdeec133486f81dd8
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2025-02-12 08:09:14 +0530

    main.py

--- main.py
+++ main.py
@@ -188,7 +188,6 @@ with col1:
 with col2:
    # st.image("https://www.vgen.it/wp-content/uploads/2021/04/logo-accenture-ludo.png", width=150)
     st.markdown("")
-
     st.markdown("""
         <style>
             @keyframes gradientAnimation {
@@ -197,19 +196,21 @@ with col2:
                 100% { background-position: 0% 50%; }
             }
     
-            # .animated-gradient-text {
-            #     font-size: 40px !important;
-            #     font-family: Arial, sans-serif !important;
-            #     font-weight: bold !important;
-            #     text-align: center;
-            #     display: block;
-            #     margin: 20px auto;
-            #     color: white; /* Fallback color */
-            # }
+            .animated-gradient-text {
+                font-size: 40px !important;
+                font-family: Arial, sans-serif !important;
+                font-weight: bold !important;
+                position: relative;
+                display: inline-block;
+                text-align: center;
+                color: white; /* Fallback color */
+            }
     
             .animated-gradient-text::before {
                 content: attr(data-text);
                 position: absolute;
+                top: 0;
+                left: 0;
                 width: 100%;
                 height: 100%;
                 background: linear-gradient(45deg, rgb(245, 58, 126) 30%, rgb(200, 1, 200) 55%, rgb(197, 45, 243) 20%);
@@ -224,6 +225,7 @@ with col2:
             Generative AI PoCs
         </p>
     """, unsafe_allow_html=True)
+
 
 
 
