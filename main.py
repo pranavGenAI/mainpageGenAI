004ec6764b3f96fe77fd09c96588313cf335e187
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2025-02-12 08:01:07 +0530

    main.py

--- main.py
+++ main.py
@@ -200,6 +200,7 @@ with col2:
             .animated-gradient-text {
                 font-size: 24px;
                 font-family: Arial, sans-serif;
+                font-weight: bold;
                 background: linear-gradient(45deg, rgb(245, 58, 126) 30%, rgb(200, 1, 200) 55%, rgb(197, 45, 243) 20%);
                 background-size: 300% 200%;
                 -webkit-background-clip: text;
